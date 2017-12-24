b830e902785260240f35b968279686bbfacc500d
Shawn Andrews <shawnandrews@live.ca>
2017-12-24 00:23:13 -0600

    Fix less

--- client/src/dashboard/dashboard.jsx
+++ client/src/dashboard/dashboard.jsx
@@ -136,7 +136,7 @@ class Dashboard extends React.Component {
     return (
         <AppBar
             title={<span>Dashboard</span>}
-            className={styles.dashboard}
+            style={styles.dashboard}
             titleStyle={styles.titleText}
             iconElementLeft={
                 <IconMenu
