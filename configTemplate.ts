1e695f90dd51c96002ea98877271bd9e11d0edae
Shawn Andrews <shawnandrews@live.ca>
2018-09-14 20:35:55 -0600

    Test travis

---
+++ configTemplate.ts
@@ -0,0 +1,45 @@
+import { Config } from "./client/client-server-common/common";
+
+const config: Config = {
+    useStrictlyHttps: true,
+    httpPort: 80,
+    httpsPort: 443,
+    chatPort: 81,
+    chatHistoryCount: 100,
+    connectionStrings: {
+        local: "",
+        remote: "",
+    },
+    token_expiration: 900000, // 15m
+    token_remember_expiration: 86400000, // 1day
+    token_length: 32, // 32characters
+    igdb: {
+        key: "",
+        pageLimit: 50
+    },
+    steam: {
+        apiURL: "http://store.steampowered.com/api",
+        nonApiURL: "http://store.steampowered.com",
+        appURL: "http://store.steampowered.com/app",
+        key: ""
+    },
+    twitch: {
+        clientId: ""
+    },
+    imgur: {
+        clientId: ""
+    },
+    smtp: {
+        host: "",
+        secure: false,
+        username: "",
+        password: ""
+    },
+    https: {
+        key: `./key.pem`,
+        cert: `./certificate.crt`,
+        ca: `./bundle.ca-bundle`
+    }
+};
+
+export default config;
