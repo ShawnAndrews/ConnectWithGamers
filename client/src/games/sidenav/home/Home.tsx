9f024e7277e494a2029d9393d286487d0de871d3
Shawn Andrews <shawnandrews@live.ca>
2019-02-12 09:54:26 -0600

    added icons to home options

--- client/src/games/sidenav/home/Home.tsx
+++ client/src/games/sidenav/home/Home.tsx
@@ -21,18 +21,23 @@ const Home: React.SFC<IHomeProps> = (props: IHomeProps) => {
             <div className="px-3 my-3">
                 <div className="title mb-2">Browse</div>
                 <div className={`option py-1 color-tertiary ${props.isSelected(HomeOptionsEnum.MostPopular) ? 'selected' : ''}`} onClick={() => props.onOptionClick(HomeOptionsEnum.MostPopular)}>
+                    <i className="fas fa-fire mr-2"/>
                     Most Popular
                 </div>
                 <div className={`option py-1 color-tertiary ${props.isSelected(HomeOptionsEnum.MostPopular) ? 'selected' : ''}`} onClick={() => props.onOptionClick(HomeOptionsEnum.RecentlyReleased)}>
+                    <i className="far fa-calendar-alt mr-2"/>
                     Recently Released
                 </div>
                 <div className={`option py-1 color-tertiary ${props.isSelected(HomeOptionsEnum.MostPopular) ? 'selected' : ''}`} onClick={() => props.onOptionClick(HomeOptionsEnum.Upcoming)}>
+                    <i className="far fa-clock mr-2"/>
                     Upcoming
                 </div>
                 <div className={`option py-1 color-tertiary ${props.isSelected(HomeOptionsEnum.MostPopular) ? 'selected' : ''}`} onClick={() => props.onOptionClick(HomeOptionsEnum.IOSComingSoon)}>
+                    <i className="fab fa-app-store-ios mr-2"/>
                     IOS Apps Coming Soon
                 </div>
                 <div className={`option py-1 color-tertiary ${props.isSelected(HomeOptionsEnum.MostPopular) ? 'selected' : ''}`} onClick={() => props.onOptionClick(HomeOptionsEnum.AndroidComingSoon)}>
+                    <i className="fab fa-google-play mr-2"/>
                     Android Apps Coming Soon
                 </div>
             </div>
